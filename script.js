2d1c4ced43dbde2bd8d302f91769bc2eda1a67f6
Ritik kotgiriwar <87945415+Kotgiriwarritik@users.noreply.github.com>
2023-12-10 22:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+        var firstInput = parseFloat(prompt("First variable:"));
+        var result = firstInput * 2;
+        var secondInput = parseFloat(prompt("Second variable:"));
+
+        result = result + secondInput;
+  alert("The final result is: " + result);
+
+//  let first =  prompt("First Input");
+//  let second = prompt("Second Input");  
+
+// let result = 2*first;
+// let f = result+second;
+// alert("The final result is: "+f);
+
